31326f923dda2acae160462ca433f81ef6cae128
Mirza Noman <nomanisthebest2004@gmail.com>
2026-09-10 20:24:48 +0600

    Initial commit

---
+++ src/app.ts
@@ -0,0 +1,61 @@
+const audio = document.getElementById("audio-element") as HTMLAudioElement;
+const playButton = document.getElementById("play-btn") as HTMLButtonElement;
+const pauseButton = document.getElementById("pause-btn") as HTMLButtonElement;
+const stopButton = document.getElementById("stop-btn") as HTMLButtonElement;
+const trackStatus = document.getElementById("track-status") as HTMLElement;
+const trackTitle = document.getElementById("track-title") as HTMLElement;
+const disc = document.getElementById("disc") as HTMLImageElement;
+audio.src = 'https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3';
+
+let angle = 0;
+const FPS = 60;
+let isPlaying = false;
+function rotateDisc(): void{
+    if (isPlaying === false) {
+        return;
+    }
+    disc.style.transform = `rotate(${angle}deg)`;
+    angle++;
+    if (angle >= 360) {
+        angle = 0;
+    }
+    requestAnimationFrame(rotateDisc)
+}
+function setDiscToZero() {
+    if (angle >= 180) {
+        angle += 2;
+    } else {
+        angle -= 2;
+    }
+    disc.style.transform = `rotate(${angle}deg)`;
+    if (angle <= 0 || angle >=  360 || isPlaying === true) {
+        disc.style.transform = `rotate(${angle}deg)`;
+        return;
+    }
+    requestAnimationFrame(setDiscToZero);
+
+}
+playButton.addEventListener('click', ()=> {
+    audio.play();
+    trackStatus.innerText = `Playing`;
+    trackTitle.innerText = `Track 1`;
+    if (isPlaying === false){
+        isPlaying = true;
+        requestAnimationFrame(rotateDisc);
+    }
+});
+pauseButton.addEventListener('click', ()=> {
+    isPlaying = false;
+    audio.pause();
+    trackStatus.innerText = `Paused`;
+});
+stopButton.addEventListener('click', ()=> {
+    audio.pause();
+    audio.currentTime = 0;
+    if (isPlaying === true) {
+        isPlaying = false;
+        requestAnimationFrame(setDiscToZero);
+    }
+    trackStatus.innerText = `Stopped`;
+    trackTitle.innerText = `No Track Loaded`;
+});
